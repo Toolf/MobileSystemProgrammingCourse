b2923f9ffda9d6a443ca21493b50889d3aa00fb3
Toolf <nazar.smirnov.2001@gmail.com>
2021-02-09 16:56:15 +0200

    Remove prints from local_book_service =)

--- lib/services/local_book_service.dart
+++ lib/services/local_book_service.dart
@@ -9,21 +9,16 @@ class LocalBookService implements BookService {
   @override
   Future<List<Book>> getBooks() async {
     try {
-      // final file = await _localFile;
-
       // Read the file.
       String data = await rootBundle.loadString('assets/BooksList.txt');
-      print(data);
       dynamic jsonBooks = jsonDecode(data);
       List<Book> books = List();
 
       for (dynamic book in jsonBooks['books']) {
         books.add(Book.fromJson(book));
       }
-      print(data);
       return books;
     } catch (e) {
-      print(e);
       // If encountering an error, return [].
       return [];
     }
