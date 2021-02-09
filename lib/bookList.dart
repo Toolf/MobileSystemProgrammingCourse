6f6c19fbb2c34999189988c2f2aaeb1e95ff5efb
Toolf <nazar.smirnov.2001@gmail.com>
2021-02-09 16:55:10 +0200

    Change books view list. Solve the problem with defunct images

--- lib/bookList.dart
+++ lib/bookList.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/services/book_service.dart';
 import 'package:flutter_application_1/services/local_book_service.dart';
 
@@ -14,56 +17,74 @@ class BookList extends StatefulWidget {
 }
 
 class _BookListState extends State<BookList> {
+  Future<Widget> _getImage(String path) async {
+    if (path == null) {
+      return SizedBox.shrink();
+    }
+
+    try {
+      await rootBundle.load(path);
+      return Image.asset(path);
+    } catch (_) {
+      return SizedBox.shrink();
+    }
+  }
+
+  Future<Widget> buildItem(Book book) async {
+    Widget image;
+    if (book.image != "") {
+      image = await _getImage('assets/Images/${book.image}');
+    } else {
+      image = await _getImage(null); // return default image
+    }
+    return ListTile(
+      isThreeLine: true,
+      title: Text(book.title),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(book.subtitle),
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Text(book.price),
+          ),
+        ],
+      ),
+      leading: Container(
+        child: image,
+        width: 50,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: FutureBuilder(
-        future: widget.bookService.getBooks(),
-        builder: (BuildContext context, AsyncSnapshot<List<Book>> snapshot) {
+      body: FutureBuilder<List<Widget>>(
+        future: Future(() async {
+          List<Book> books = await widget.bookService.getBooks();
+          List<Widget> bookWidgets = List();
+          for (Book book in books) {
+            bookWidgets.add(await buildItem(book));
+          }
+          return bookWidgets;
+        }),
+        builder: (BuildContext context, AsyncSnapshot<List<Widget>> snapshot) {
           if (!snapshot.hasData) {
             return Center(child: CircularProgressIndicator());
           }
 
-          List<Book> books = snapshot.data;
-          return SingleChildScrollView(
-            child: Table(
-              columnWidths: {0: FractionColumnWidth(0.3)},
-              children: books.map(
-                (Book book) {
-                  Widget image = book.image != null
-                      ? Image.asset(
-                          './assets/Images/${book.image}',
-                        )
-                      : Container();
-                  return TableRow(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [image],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              book.title,
-                            ),
-                            Text(book.subtitle),
-                            Text(book.isbn13),
-                            Text(book.price),
-                          ],
-                        ),
-                      )
-                    ],
-                  );
-                },
-              ).toList(),
-            ),
+          List<Widget> books = snapshot.data;
+          return ListView.separated(
+            itemCount: books.length,
+            separatorBuilder: (BuildContext context, int index) => Divider(),
+            itemBuilder: (BuildContext context, int index) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return books[index];
+            },
           );
         },
       ),
