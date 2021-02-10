468690881346dd88ff5607e5cf72f49fca9eefd3
Toolf <nazar.smirnov.2001@gmail.com>
2021-02-10 21:05:51 +0200

    Upgrade model Book for new api

--- lib/models/api_models.dart
+++ lib/models/api_models.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-// "subtitle":"",
-//         "isbn13":"noid",
-//         "price":"Priceless",
-//         "image":""}
 
 @immutable
 class Book {
@@ -10,14 +6,27 @@ class Book {
   final String subtitle;
   final String isbn13;
   final String price;
-  final String image;
+  final Image image;
+
+  final String authors;
+  final String publisher;
+  final String pages;
+  final String year;
+  final String rating;
+  final String desc;
 
   const Book({
     @required this.title,
     @required this.subtitle,
-    @required this.isbn13,
     @required this.price,
-    @required this.image,
+    this.isbn13,
+    this.image,
+    this.publisher,
+    this.pages,
+    this.year,
+    this.rating,
+    this.desc,
+    this.authors,
   });
 
   Book.fromJson(dynamic json)
@@ -26,7 +35,15 @@ class Book {
           subtitle: json['subtitle'],
           isbn13: json['isbn13'],
           price: json['price'],
-          image: json['image'],
+          image: json['image'] != ""
+              ? Image.asset('assets/Images/${json['image']}')
+              : null,
+          publisher: json['publisher'],
+          pages: json['pages'],
+          year: json['year'],
+          rating: json['rating'],
+          desc: json['desc'],
+          authors: json['authors'],
         );
 
   @override
