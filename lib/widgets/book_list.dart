76588eb9e7c84d1388a5d11d657ae9c69595e418
Toolf <nazar.smirnov.2001@gmail.com>
2021-02-10 21:04:55 +0200

    Add widgets for search, add, delete, details functionality for book

--- lib/widgets/book_list.dart
+++ lib/widgets/book_list.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_application_1/services/book_service.dart';
-import 'package:flutter_application_1/services/local_book_service.dart';
+
 import 'package:flutter_slidable/flutter_slidable.dart';
 
-import 'models/api_models.dart';
+import '../pages/book_add.dart';
+import '../pages/book_details.dart';
+import '../services/book_service.dart';
+import '../services/local_book_service.dart';
+import '../models/api_models.dart';
 
 class BookList extends StatefulWidget {
   final BookService bookService = LocalBookService();
@@ -18,8 +20,8 @@ class BookList extends StatefulWidget {
 class _BookListState extends State<BookList> {
   List<Book> books = List();
   List<Widget> bookWidgets = List();
-
-  final GlobalKey<AnimatedListState> _anim = GlobalKey();
+  List<Widget> showWidgets = List();
+  String searchLine = "";
 
   @override
   void initState() {
@@ -32,6 +34,7 @@ class _BookListState extends State<BookList> {
       setState(() {
         this.books = books;
         this.bookWidgets = bookWidgets;
+        this.showWidgets = bookWidgets.toList();
       });
     });
   }
@@ -39,47 +42,79 @@ class _BookListState extends State<BookList> {
   @override
   Widget build(BuildContext context) {
     if (books.isEmpty) {
-      return Container();
+      return Center(
+        child: CircularProgressIndicator(),
+      );
     }
 
     return Scaffold(
-      appBar: AppBar(),
-      body: AnimatedList(
-        key: _anim,
-        initialItemCount: bookWidgets.length,
-        itemBuilder: (context, index, animation) {
-          return SizeTransition(
-            sizeFactor: animation,
-            child: bookWidgets[index],
-          );
-        },
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+          child: TextField(
+            cursorColor: Colors.white,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              errorBorder: InputBorder.none,
+              disabledBorder: InputBorder.none,
+              contentPadding:
+                  EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+              labelText: "Search",
+              labelStyle: TextStyle(color: Colors.white),
+            ),
+            onChanged: (value) {
+              setState(() {
+                searchLine = value;
+                showWidgets = bookWidgets
+                    .where((w) =>
+                        books[bookWidgets.indexOf(w)].title.contains(value))
+                    .toList();
+              });
+            },
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(_bookAddPageRoute());
+              },
+              child: Icon(Icons.add),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            child: showWidgets.length != 0
+                ? ListView.builder(
+                    itemCount: showWidgets.length,
+                    itemBuilder: (context, index) => showWidgets[index],
+                  )
+                : Center(
+                    child: Text("No items found"),
+                  ),
+          ),
+        ],
       ),
     );
   }
 
-  Future<Widget> _getImage(String path) async {
-    if (path == null) {
-      return SizedBox.shrink();
-    }
-
-    try {
-      await rootBundle.load(path);
-      return Image.asset(path);
-    } catch (_) {
-      return SizedBox.shrink();
-    }
-  }
-
   Future<Widget> _buildItem(Book book) async {
     Widget image;
-    if (book.image != "") {
-      image = await _getImage('assets/Images/${book.image}');
+    if (book.image == null) {
+      image = SizedBox.shrink(); // return default image
     } else {
-      image = await _getImage(null); // return default image
+      image = book.image;
     }
     return Slidable(
       key: Key(book.title),
-      actionPane: SlidableBehindActionPane(),
+      actionPane: SlidableScrollActionPane(),
       child: Container(
         color: Colors.white,
         child: ListTile(
@@ -99,6 +134,16 @@ class _BookListState extends State<BookList> {
             child: image,
             width: 50,
           ),
+          onTap: () async {
+            Book bookMore = await widget.bookService.getBook(book.isbn13);
+            if (bookMore != null) {
+              Navigator.of(context).push(_createRoute(bookMore));
+            } else {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                  duration: const Duration(seconds: 1),
+                  content: Text("Not found resurce")));
+            }
+          },
         ),
       ),
       secondaryActions: [
@@ -106,7 +151,7 @@ class _BookListState extends State<BookList> {
           caption: 'Delete',
           color: Colors.red,
           icon: Icons.delete,
-          onTap: () async {
+          onTap: () {
             _removeBook(book);
           },
         ),
@@ -114,24 +159,70 @@ class _BookListState extends State<BookList> {
     );
   }
 
-  void _removeBook(book) async {
-    Widget bookWidget = await _buildItem(book);
-    AnimatedListRemovedItemBuilder builder = (context, animation) {
-      return SizeTransition(
-        sizeFactor: animation,
-        child: Opacity(opacity: 0.0, child: bookWidget),
-      );
-    };
-
+  void _removeBook(book) {
     var index = books.indexOf(book);
-    _anim.currentState.removeItem(
-      index,
-      builder,
-    );
 
     setState(() {
+      if (showWidgets.contains(bookWidgets[index])) {
+        showWidgets.removeAt(showWidgets.indexOf(bookWidgets[index]));
+      }
       books.removeAt(index);
       bookWidgets.removeAt(index);
     });
+  }
+
+  void _addBook(Book book) async {
+    Widget w = await _buildItem(book);
+
+    setState(() {
+      books.add(book);
+      bookWidgets.add(w);
+      if (book.title.contains(searchLine)) {
+        showWidgets.add(w);
+      }
+    });
+  }
+
+  Route _createRoute(Book book) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          BookPage(book: book),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(1.0, 0.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
+  Route _bookAddPageRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return BookAdd(
+          onValid: (book) => _addBook(book),
+        );
+      },
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(1.0, 0.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
   }
 }
